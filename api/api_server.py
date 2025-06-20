f80e064d58b7ce384454e9adf94670b7fb95036c
Krusovice <jmk1589@gmail.com>
2025-06-20 08:48:30 +0200

    fastapi container implemented

--- api/api_server.py
+++ api/api_server.py
@@ -2,23 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
-from src.ml.feature_engineering import create_features_soil_layers
 
-model = joblib.load("./model.pkl")
+model = joblib.load("./models/linear_model_latest.pkl")
 
 # Define input structure (adjust fields to match your training input)
 class InputData(BaseModel):
     width: float
     eccentricity: float
     soil_model: str
-    soil_modulus: list
+    soil_modulus: list[float]
     
 
 app = FastAPI()
 
 @app.post("/predict")
 def predict(data: InputData):
-
     df = pd.DataFrame([data.dict()])
     prediction = model.predict(df)
     return {"prediction": float(prediction[0])}
